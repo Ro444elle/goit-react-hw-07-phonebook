94e2f207ffe22f7670b3e1da726bb1a7fb03a70c
Ro444elle <121978303+Ro444elle@users.noreply.github.com>
2023-09-30 14:27:38 +0300

    Commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
-  fetchContactsError,
+  // fetchContactsError,
   fetchContactsSucces,
   setFilter,
-  fetchContactsStart,
+  // fetchContactsStart,
   createContact,
 } from 'redux/contactsSlice';
 import { fetchContacts } from './redux/operations';
@@ -13,28 +13,18 @@ import ContactList from 'components/ContactList/ContactList';
 import Filter from './components/ContactList/Filter/Filter';
 import styles from './App.module.css';
 import { contactApi } from 'api/api';
-
 export default function App() {
   const dispatch = useDispatch();
   const contacts = useSelector(state => state.contacts.items);
   const filter = useSelector(state => state.contacts.filter);
-
   useEffect(() => {
-    console.log('Filter:', filter);
-    console.log('Contacts:', contacts);
+    // console.log('Filter:', filter);
+    // console.log('Contacts:', contacts);
     const fetchData = async () => {
-      dispatch(fetchContactsStart());
-      try {
-        const response = await fetchContacts();
-        dispatch(fetchContactsSucces(response.data));
-      } catch (error) {
-        dispatch(fetchContactsError(error.message));
-      }
+      dispatch(fetchContacts());
     };
-
     fetchData();
   }, [dispatch, contacts, filter]);
-
   const handleAddContact = async newContact => {
     try {
       const response = await createContact(newContact);
@@ -43,7 +33,6 @@ export default function App() {
       console.log('Error creating contact:', error);
     }
   };
-
   const handleDeleteContact = async contactId => {
     try {
       await contactApi.delete(contactId);
@@ -52,22 +41,18 @@ export default function App() {
       console.error('Error deleting contact:', error);
     }
   };
-
   const handleFilterChange = value => {
     dispatch(setFilter(value));
   };
-
   return (
     <div className="App">
       <div className={styles.phonebook}>
         <h1>Phonebook</h1>
         <ContactForm onSubmit={handleAddContact} />
       </div>
-
       <div className={styles.contacts}>
         <h2>Contacts</h2>
         <Filter value={filter} onChange={handleFilterChange} />
-
         <ContactList
           contacts={contacts}
           filter={filter}
